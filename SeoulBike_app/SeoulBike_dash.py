1cde1ea3d0dc4cdd4ba64bb4cf9298d6c12c4fca
xinyao qian <zceexqi@ucl.ac.uk>
2022-02-03 02:18:43 +0000

    dash app draft finished

--- SeoulBike_app/SeoulBike_dash.py
+++ SeoulBike_app/SeoulBike_dash.py
@@ -76,7 +76,7 @@ navbar = dbc.Navbar(
 
         ]
     ),
-    color="dark",
+    color="lightblue",
     dark=True,
 )
 
@@ -85,15 +85,23 @@ navbar = dbc.Navbar(
 # ------Import data for visualization------
 df = pd.read_csv('data/Prepared_data.csv')
 
-seasons = df.groupby('Seasons')['Rented Bike Count'].sum().reset_index(name ='Total Amount')
-hour = df.groupby('Hour').sum()['Rented Bike Count'].reset_index(name ='Total Amount')
-temp_rent = df.groupby('Temperature').mean()['Rented Bike Count'].reset_index(name ='Rented Bike Count')
+df['Date'] = pd.to_datetime(df['Date'], format="%Y/%m/%d")
 
-#Temperature line plot
-temp_line=px.line(temp_rent,x='Temperature',y='Rented Bike Count',title='Rented Bike Count vs Temperature')
+df['month'] = df['Date'].dt.month
+
+seasons = df.groupby('Seasons')['Rented Bike Count'].sum().reset_index(
+    name='Total Amount')
+hour = df.groupby('Hour').sum()['Rented Bike Count'].reset_index(
+    name='Total Amount')
+temp_rent = df.groupby('Temperature').mean()['Rented Bike Count'].reset_index(
+    name='Rented Bike Count')
+
+# Temperature line plot
+temp_line = px.line(temp_rent, x='Temperature', y='Rented Bike Count',
+                    title='Rented Bike Count vs Temperature')
 temp_line.update_layout(title_text='Your title', title_x=0.5)
-#Season bar plot
-season_total_bar=px.bar(seasons,x='Seasons',y='Total Amount')
+# Season bar plot
+season_total_bar = px.bar(seasons, x='Seasons', y='Total Amount')
 season_total_bar.update_layout(title_text='Your title', title_x=0.5)
 # Hourly line and bar plot
 hour_line_bar = go.Figure()
@@ -107,34 +115,34 @@ hour_line_bar.add_trace(
     go.Bar(
         x=hour['Hour'],
         y=hour['Total Amount'],
+
     ))
 hour_line_bar.update_layout(
     title={
         'text': "Plot Title",
-        'y':0.9,
-        'x':0.5,
+        'y': 0.9,
+        'x': 0.5,
         'xanchor': 'center',
         'yanchor': 'top'})
 
 # Day and Night Pie plot
-day_pie = go.Figure(data=[go.Pie(labels=df['Day_night'], values=df['Rented Bike Count'])])
+day_pie = go.Figure(
+    data=[go.Pie(labels=df['Day_night'], values=df['Rented Bike Count'])])
 day_pie.update_traces(hole=.4, hoverinfo="label+percent+name")
-#day_pie.update_layout(title='Title')
+# day_pie.update_layout(title='Title')
 
 day_pie.update_layout(
     title={
         'text': "Plot Title",
-        'y':0.9,
-        'x':0.5,
+        'y': 0.9,
+        'x': 0.5,
         'xanchor': 'center',
         'yanchor': 'top'})
 
-
-
-#Elements in layout
-title=html.H1(
+# Elements in layout
+title = html.H1(
     children='DATA DASHBOARD',
-    style={'text-align':'center'}
+    style={'text-align': 'center'}
 )
 
 day_night_pie = html.Div(
@@ -143,7 +151,7 @@ day_night_pie = html.Div(
         figure=day_pie,
     )
 )
-bar_line=html.Div(
+bar_line = html.Div(
     children=dcc.Graph(
         id='bar_line',
         figure=hour_line_bar,
@@ -151,17 +159,17 @@ bar_line=html.Div(
 
 )
 
-season_bar= html.Div(
+season_bar = html.Div(
     children=dcc.Graph(
-    id='seasonal amount',
-    figure=season_total_bar,
+        id='seasonal amount',
+        figure=season_total_bar,
 
-))
-temperature_line=html.Div(
+    ))
+temperature_line = html.Div(
     children=dcc.Graph(
         id='temp_line',
         figure=temp_line,
-)
+    )
 )
 
 card = dbc.Card(
@@ -181,6 +189,7 @@ card = dbc.Card(
     ],
     style={"width": "30rem"},
 )
+
 # row of graphs
 
 row1 = dbc.Row([  # row1
@@ -195,22 +204,46 @@ row2 = dbc.Row([  # row2
     dbc.Col('Figure Expected', width=3),
 ])
 
+month_dropdown = dcc.Dropdown(id='month-dropdown',
+                              options=[{'label': month, 'value': month}
+                                       for month in df['month'].unique()],
+                              value=1)
+
+
+# dff = df.copy()
+# dff = dff[dff["month"] == 3]
+# dff = dff.groupby('Date')['Rented Bike Count'].sum().reset_index('Date')
+# fig2 = px.bar(dff, x='Date', y='Rented Bike Count')
+
 
 app.layout = html.Div(children=[
     navbar,
     title,
+    month_dropdown,
     row1,
     row2,
-    #card,
-    html.Button(id = 'html-button', children = 'Click the button !', n_clicks = 0),
-    html.Div(id='output-text'),
+    dcc.Graph(id='out1', figure={}),
+    # dcc.Graph(figure=fig2),
+
+
 ])
 
-@app.callback(Output(component_id='output-text',component_property='children'),
-              Input(component_id='html-button',component_property='n_clicks'))
-def buttom_update(value):
-    return html.Div(str(value) + ' clicks !')
+
+@app.callback([Output(component_id='out1', component_property='figure')],
+              [Input(component_id='month-dropdown',
+                     component_property='value')])
+def update_month_graph(option_selected):
+    print(option_selected)
+    print(type(option_selected))
+    container = "The month chosen by user was: {}".format(option_selected)
+    dff = df.copy()
+    dff = dff[dff["month"] == option_selected]
+    print(dff.info(3))
+    dff = dff.groupby('Date')['Rented Bike Count'].sum().reset_index('Date')
+    print(dff.info(3))
+    fig = px.bar(dff, x='Date', y='Rented Bike Count')
+    return fig
 
 
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    app.run_server(debug=True,port=3000)
